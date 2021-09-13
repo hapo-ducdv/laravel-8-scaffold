8d2c271a5f25f72e92c08644258722a54b4bd674
Hapo Ducdv <duc.dv@haposoft.com>
2021-09-13 08:04:25 +0700

    First commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,257 @@
 @extends('layouts.app')
 
+@section('title', 'Home')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="cih">
+        <div class="d-flex align-items-center banner-first ">
+            <div class="banner-first-content">
+                <dic class="banner-first-content-heading d-block">Learn Anytime, Anywhere</dic>
+                <div class="banner-first-content-bold d-block">at HapoLearn
+                    <img src="{{ asset('/images/icon-hapo.png') }}" alt="Icon hapo">!
+                </div>
+                <div class="banner-first-content-desc-up d-block">Interface lessons, "on-the-go" practice,</div>
+                <div class="banner-first-content-desc-bot d-block">peer support</div>
+                <a class="btn d-flex align-items-center justify-content-center" href="#">Start Learning Now !</a>
+            </div>
+        </div>
+        <div class="linear-gradient cih">
+            <div class="transition-block"></div>
+        </div>
+    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container courses cih">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-img-top d-flex align-items-center justify-content-center html-css-js">
+                        <img src="{{ asset('/images/html-css-js.png') }}" alt="Html, css & js">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Html/Css/js Tutorial</h5>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                        <a href="#" class="btn">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-img-top d-flex align-items-center justify-content-center laravel">
+                        <img src="{{ asset('/images/laravel-1-white.png') }}" alt="Laravel">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">LARAVEL Tutorial </h5>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                        <a href="#" class="btn">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-img-top d-flex align-items-center justify-content-center php">
+                        <img src="{{ asset('/images/php-logo.png') }}" alt="Php">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">PHP Tutorial</h5>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                        <a href="#" class="btn">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <span class="other_courses">Other courses</span>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-img-top d-flex align-items-center justify-content-center css">
+                        <img src="{{ asset('/images/css.png') }}" alt="Html, css & js">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">CSS Tutorial</h5>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media,...</p>
+                        <a href="#" class="btn">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-img-top d-flex align-items-center justify-content-center rails">
+                        <img src="{{ asset('/images/rails.png') }}" alt="Laravel">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ruby on rails Tutorial</h5>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media,...</p>
+                        <a href="#" class="btn">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-img-top d-flex align-items-center justify-content-center java1">
+                        <img src="{{ asset('/images/java1.png') }}" alt="Php">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Java Tutorial</h5>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media,...</p>
+                        <a href="#" class="btn">Take This Course</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <a class="row view-all" href="#">View All Our Courses
+                <i class="fas fa-long-arrow-alt-right d-flex arrow-right"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="container-fluid align-items-center why-hapolearn cih">
+        <div class="row align-items-center">
+            <div class="why-hapolearn-text col-md-6 d-flex flex-column">
+                <div class="why-hapolearn-title">Why HapoLearn?</div>
+                <div class="text-content">
+                    <i class="fas fa-check-circle"></i><span>Interactive lessons, "on-the-go" practice, peer support.</span>
+                </div>
+                <div class="text-content">
+                    <i class="fas fa-check-circle"></i><span>Interactive lessons, "on-the-go" practice, peer support.</span>
+                </div>
+                <div class="text-content">
+                    <i class="fas fa-check-circle"></i><span>Interactive lessons, "on-the-go" practice, peer support.</span>
+                </div>
+                <div class="text-content">
+                    <i class="fas fa-check-circle"></i><span>Interactive lessons, "on-the-go" practice, peer support.</span>
+                </div>
+                <div class="text-content">
+                    <i class="fas fa-check-circle"></i><span>Interactive lessons, "on-the-go" practice, peer support.</span>
+                </div>
+            </div>
+            <div class="multi-device-img col-md-6 d-flex align-items-center">
+                <img src="{{ asset('/images/multi-device-img.png') }}" alt="Multi device img">
+            </div>
+        </div>
+        <div class="mb-img">
+            <img src="{{ asset('/images/mb.png') }}" alt="mb">
+        </div>
+    </div>
+
+    <div class="container feedback cih">
+        <div class="feedback-title text-center">
+            <span class="feedback-title-text">Feedback</span>
+        </div>
+        <div class="feedback-intro">
+            <p>What other students turned professionals have to say about us after learning with us and reaching their goals</p>
+        </div>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner feedback-slide">
+                <div class="carousel-item active">
+                    <div class="feedback-border">
+                        <div class="feedback-border-text">“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”</div>
+                    </div>
+                    <div class="feedback-abc-border">
+                        <img src="{{ asset('/images/polygon.png') }}" alt="">
+                    </div>
+                    <div class="feedback-info d-flex">
+                        <img src="{{ asset('/images/avatar.png') }}" class="rounded-circle feedback-info-img" alt="Avatar">
+                        <div class="card-body">
+                            <div class="feedback-info-title">Hoang Anh Nguyen</div>
+                            <div class="feedback-info-subtitle">PHP</div>
+                            <div class="feedback-info-icon">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="feedback-border">
+                        <div class="feedback-border-text">“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”</div>
+                    </div>
+                    <div class="feedback-info d-flex">
+                        <img src="{{ asset('/images/avatar.png') }}" class="rounded-circle feedback-info-img" alt="Avatar">
+                        <div class="card-body">
+                            <div class="feedback-info-title">Hoang Anh Nguyen</div>
+                            <div class="feedback-info-subtitle">Python</div>
+                            <div class="feedback-info-icon">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="feedback-border">
+                        <div class="feedback-border-text">“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”</div>
+                    </div>
+                    <div class="feedback-info d-flex">
+                        <img src="{{ asset('/images/avatar.png') }}" class="rounded-circle feedback-info-img" alt="Avatar">
+                        <div class="card-body">
+                            <div class="feedback-info-title">Hoang Anh Nguyen</div>
+                            <div class="feedback-info-subtitle">Java</div>
+                            <div class="feedback-info-icon">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="feedback-border">
+                        <div class="feedback-border-text">“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”</div>
+                    </div>
+                    <div class="feedback-info d-flex">
+                        <img src="{{ asset('/images/avatar.png') }}" class="rounded-circle feedback-info-img" alt="Avatar">
+                        <div class="card-body">
+                            <div class="feedback-info-title">Hoang Anh Nguyen</div>
+                            <div class="feedback-info-subtitle">Java</div>
+                            <div class="feedback-info-icon">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="cih">
+        <div class="d-flex flex-column align-items-center justify-content-center banner-end">
+            <div class="text-center banner-end-text">Become a member of our growing community!</div>
+            <a class="btn" href="#">Start Learning Now!</a>
+        </div>
+    </div>
+
+    <div class="container statistic cih">
+        <div class="row statistic-title justify-content-center">
+            <span class="statistic-title-underline">Statistic</span>
+        </div>
+        <div class="row statistic-content">
+            <div class="col-12 col-md-4 d-flex flex-column align-items-center">
+                <div class="statistic-content-title">Courses</div>
+                <div class="statistic-content-number">1586</div>
+            </div>
+            <div class="col-12 col-md-4 d-flex flex-column align-items-center">
+                <div class="statistic-content-title">Lessons</div>
+                <div class="statistic-content-number">2,689</div>
+            </div>
+            <div class="col-12 col-md-4 d-flex flex-column align-items-center">
+                <div class="statistic-content-title">Learns</div>
+                <div class="statistic-content-number">16,882</div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,34 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>@yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('images/icon-hapo.png') }}" type="image/png">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+@include('partials.header')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+@yield('content')
 
-                    </ul>
+@include('partials.footer')
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+@include('auth.login-register')
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+@include('partials.chatbox')
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js" integrity="sha512-WNZwVebQjhSxEzwbettGuQgWxbpYdoLf7mH+25A7sfQbbxKeS5SQ9QBf97zOY4nOlwtksgDA/czSTmfj4DUEiQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="{{ asset('js/index.js') }}"></script>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
 </body>
 </html>
