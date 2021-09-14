bf873dcafd256c3cbc8b0d7dbba599fbc015900d
Hapo Ducdv <duc.dv@haposoft.com>
2021-09-14 08:44:00 +0700

    Remove folders and files

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-require('./bootstrap');
-require('./index');
-
-import "slick-carousel/slick/slick.min.js";
-import "slick-carousel/slick/slick.js";
-
-import $ from "jquery";
-window.$ = window.jQuery = $;
