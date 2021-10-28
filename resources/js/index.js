37cfa00c723864b6429551c824b090f685a01749
Hapo Ducdv <duc.dv@haposoft.com>
2021-10-28 16:05:30 +0700

    fix code 1

--- resources/js/index.js
+++ resources/js/index.js
@@ -72,11 +72,11 @@
   });
 
   $("ul.nav-pills > li > a").on("shown.bs.tab", function (e) {
-    var scrollHeight = $(document).scrollTop();
+    let scrollHeight = $(document).scrollTop();
     setTimeout(function() {
       $(window).scrollTop(scrollHeight );
     }, 5);
-    var id = $(e.target).attr("href").substr(1);
+    let id = $(e.target).attr("href").substr(1);
     window.location.hash = id;
   });
 
