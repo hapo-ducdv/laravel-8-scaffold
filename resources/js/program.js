d5cb261eee115cfbd6826b4234c52e9687a3b595
Hapo Ducdv <duc.dv@haposoft.com>
2021-10-30 16:26:19 +0700

    create function base ajax setup

--- resources/js/program.js
+++ resources/js/program.js
@@ -1,18 +1,31 @@
 $(function () {
+  function baseAjaxSetup(type, link, data, dataType) {
+    $.ajaxSetup({
+      type: type,
+      url: link,
+      data: data,
+      dataType: dataType,
+      headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+      }
+    });
+  }
+
   $('.btn-preview').click(function() {
+    var ajaxSetup = baseAjaxSetup(
+      'post',
+      '/programs/join',
+      {programId: $(this).data('program-id')},
+      'json'
+      );
+
     $.ajax({
-      type: 'post',
-      url: '/programs/join',
-      data: {
-        programId: $(this).data('program-id'),
-        _token: $('meta[name="csrf-token"]').attr('content')
-      },
-      dataType: 'json',
       success: function (response) {
         $('#progressBar').css('width', response.progress + '%');
         $('#progressNumber').html(response.progress + '%');
       }
     })
+
     $(this).html('Previewed');
   });
 });
